a6a543c1a1125fa460bb36859b382d3e4e9f40ee
rio wei <wutony0713@gmail.com>
2022-08-17 15:03:11 +0800

    漢堡選單 & 首頁待修改

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,13 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
+// $(function() {
+//   console.log('Hello Bootstrap5');
+// });
+
+// const item = document.querySelector(".info .info_item");
+// const line = document.querySelector("#line");
+// const icon = document.querySelector(".info .info_icon");
+
+// item.addEventListener("mouseover",()=>{
+//   console.log(`jlfsj`);
+//   line.classList.toggle("color");
+//   icon.classList.toggle("color");
+// })
