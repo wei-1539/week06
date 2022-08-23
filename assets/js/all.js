14a37840d92360e3e866fd1bc498528c92641b14
rio wei <wutony0713@gmail.com>
2022-08-23 14:54:39 +0800

    Update 2022-08-23T06:54:27.862Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,35 +1,31 @@
+"use strict";
+
 // $(function() {
 //   console.log('Hello Bootstrap5');
 // });
-
 // const item = document.querySelector(".info .info_item");
 // const line = document.querySelector("#line");
 // const icon = document.querySelector(".info .info_icon");
-
 // item.addEventListener("mouseover",()=>{
 //   console.log(`jlfsj`);
 //   line.classList.toggle("color");
 //   icon.classList.toggle("color");
 // })
-
-
 // Swiper
-const swiper = new Swiper('.swiper', {
-  
+var swiper = new Swiper('.swiper', {
   // Optional parameters
   effect: 'fade',
   fadeEffect: {
     crossFade: true
   },
-  
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: '.swiper-pagination'
   },
-
   // Navigation arrows
   navigation: {
     nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+    prevEl: '.swiper-button-prev'
+  }
 });
+//# sourceMappingURL=all.js.map
