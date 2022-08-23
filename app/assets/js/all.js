61bc476df9631b2489b97554c0420e807b00dd9a
rio wei <wutony0713@gmail.com>
2022-08-23 14:52:44 +0800

    首頁加入Swiper & RWD 修正

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -11,3 +11,25 @@
 //   line.classList.toggle("color");
 //   icon.classList.toggle("color");
 // })
+
+
+// Swiper
+const swiper = new Swiper('.swiper', {
+  
+  // Optional parameters
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
